f4fa415b83dc4d4a2a25fee0c8f2ea71bf6ce05f
Heather McMillan <heather.o.mcmillan@gmail.com>
2021-04-03 23:28:44 -0500

    add begining of extra SQL exercises

---
+++ sql_bonus_work.sql
@@ -0,0 +1,35 @@
+-- ## Bonus Questions ## -- 
+
+-- 1. How much do the current managers of each department get paid, relative to the average salary for the department? here will operate under assumption that it is current average salary for department
+-- IS there ANY department WHERE the department manager gets paid LESS THAN the average salary?
+
+USE employees;
+
+-- find average salary per department 
+	
+SELECT 
+d.dept_name AS dept_name,
+AVG(s.salary) AS average_salary
+FROM salaries AS s
+JOIN dept_emp AS de USING(`emp_no`)
+JOIN departments AS d ON d.`dept_no` = de.`dept_no`
+WHERE s.to_date > now() AND de.to_date > now()
+GROUP BY d.dept_name;
+	
+
+ 
+-- find salaries of all CURRENT managers
+
+SELECT 
+d.dept_name AS dept_name,
+s.salary AS mngr_salary
+FROM salaries AS s
+JOIN dept_manager AS dm USING (`emp_no`)
+JOIN departments AS d ON d.dept_no = dm.dept_no
+WHERE dm.to_date > now() AND s.to_date > now();
+
+
+
+-- compare, maybe use temp table? 
+
+
